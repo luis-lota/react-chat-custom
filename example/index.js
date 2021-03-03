f6da60f8a32c7b76a89a491ea8c229d444cdf724
luis-lota <louis.miguel.05@gmail.com>
2021-03-03 16:07:42 +0000

    testing SSR

--- example/index.js
+++ example/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Example from './Example';
 
-ReactDOM.render(<Example />, document.getElementById('root'));
+// ReactDOM.render(<Example />, document.getElementById('root'));
